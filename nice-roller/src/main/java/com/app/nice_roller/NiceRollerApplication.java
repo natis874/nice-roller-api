3d93d948cc16eb0d9f9c533393089962d476db5d
TAMANT <yassine.tamant@capgemini.com>
2024-10-02 10:54:28 +0200

    initial commit

---
+++ nice-roller/src/main/java/com/app/nice_roller/NiceRollerApplication.java
@@ -0,0 +1,13 @@
+package com.app.nice_roller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceRollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceRollerApplication.class, args);
+	}
+
+}
---
+++ nice-roller/src/test/java/com/app/nice_roller/NiceRollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.nice_roller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiceRollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
