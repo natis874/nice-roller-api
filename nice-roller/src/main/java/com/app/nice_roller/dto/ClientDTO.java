45baabffa24a11bea3e6ebe4e0b9bbfd480af35b
natisenissay <yassine.tmnt@gmail.com>
2024-10-04 15:32:59 +0200

    Create ClientDTO

---
+++ nice-roller/src/main/java/com/app/nice_roller/dto/ClientDTO.java
@@ -0,0 +1,11 @@
+package com.app.nice_roller.dto;
+
+import lombok.Data;
+
+@Data
+public class ClientDTO {
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String email;
+}
