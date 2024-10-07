26ba118fa8bade9720bced619d9baf40f381b67a
natisenissay <yassine.tmnt@gmail.com>
2024-10-07 19:21:32 +0200

    Add @EnableJpaAuditing

--- nice-roller/src/main/java/com/app/nice_roller/NiceRollerApplication.java
+++ nice-roller/src/main/java/com/app/nice_roller/NiceRollerApplication.java
@@ -2,8 +2,10 @@ package com.app.nice_roller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NiceRollerApplication {
 
 	public static void main(String[] args) {
