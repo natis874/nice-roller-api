038050e7133e5f841bbb43bdf7541ffcd75f0d51
natisenissay <yassine.tmnt@gmail.com>
2025-02-24 17:58:18 +0100

    ajout les nouvelles lignes dans la base

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,27 @@
 -- Insert initial data into rollers table
-INSERT INTO rollers (marque, modele, taille, disponibilite, etat, created_by, created_date, last_modified_by, last_modified_date)
-VALUES ('Rollerblade', 'Zetrablade', '42', TRUE, 'New', 'admin', NOW(), 'admin', NOW());
+--INSERT INTO rollers (marque, modele, taille, disponibilite, etat, created_by, created_date, last_modified_by, last_modified_date)
+--VALUES ('Rollerblade', 'Zetrablade', '42', TRUE, 'New', 'admin', NOW(), 'admin', NOW());
 
+--INSERT INTO rollers (marque, modele, taille, disponibilite, etat, created_by, created_date, last_modified_by, last_modified_date)
+--VALUES ('K2', 'VO2 S 100 X', '40', FALSE, 'Used', 'admin', NOW(), 'admin', NOW());
+
+
+-- Roller 1
 INSERT INTO rollers (marque, modele, taille, disponibilite, etat, created_by, created_date, last_modified_by, last_modified_date)
-VALUES ('K2', 'VO2 S 100 X', '40', FALSE, 'Used', 'admin', NOW(), 'admin', NOW());
+VALUES ('RollerX', 'SpeedMaster', '43', TRUE, 'New', 'admin', NOW(), 'admin', NOW());
+
+-- Roller 2
+INSERT INTO rollers (marque, modele, taille, disponibilite, etat, created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('FILA', 'Kompact 84', '44', TRUE, 'New', 'admin', NOW(), 'admin', NOW());
+
+-- Roller 3
+INSERT INTO rollers (marque, modele, taille, disponibilite, etat, created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('Powerslide', 'Next 80', '45', FALSE, 'Used', 'admin', NOW(), 'admin', NOW());
+
+-- Roller 4
+INSERT INTO rollers (marque, modele, taille, disponibilite, etat, created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('Sebastian', 'Hurricane', '41', TRUE, 'New', 'admin', NOW(), 'admin', NOW());
+
+-- Roller 5
+INSERT INTO rollers (marque, modele, taille, disponibilite, etat, created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('Nike', 'Air Glide', '40', FALSE, 'Used', 'admin', NOW(), 'admin', NOW());
