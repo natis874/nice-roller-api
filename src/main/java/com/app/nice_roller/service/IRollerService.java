4458cbe89fe072f81d18ca0b735e5a423d53600b
natisenissay <yassine.tmnt@gmail.com>
2025-02-24 17:58:47 +0100

    MAJ roller service

--- src/main/java/com/app/nice_roller/service/IRollerService.java
+++ src/main/java/com/app/nice_roller/service/IRollerService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface IRollerService {
     List<RollerDTO> getAllRollers();
     RollerDTO getRollerById(Long id);
-    RollerDTO saveRoller(RollerDTO rollerDTO);
+    RollerDTO createRoller(RollerDTO rollerDTO);
     void deleteRoller(Long id);
     List<RollerDTO> getAvailableRollers();
 }
