04dca9fc4c3c80387564b418bf98c643a4fd4dca
natisenissay <yassine.tmnt@gmail.com>
2025-02-24 18:00:09 +0100

    Update RollerNotFoundException.java

--- src/main/java/com/app/nice_roller/exception/RollerNotFoundException.java
+++ src/main/java/com/app/nice_roller/exception/RollerNotFoundException.java
@@ -1,7 +1,7 @@
 package com.app.nice_roller.exception;
 
 public class RollerNotFoundException extends RuntimeException{
-    public RollerNotFoundException(Long id) {
-        super("Roller with ID " + id + " not found.");
+
+    public RollerNotFoundException(Long id) { super("Roller with ID " + id + " not found.");
     }
 }
