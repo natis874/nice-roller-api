3c6e966c0ab8a2e8467f804fbadcc38421514a85
natisenissay <yassine.tmnt@gmail.com>
2025-03-04 16:50:18 +0100

    Update ClientController.java
    Ajouter des annotations Swagger

--- src/main/java/com/app/nice_roller/controller/ClientController.java
+++ src/main/java/com/app/nice_roller/controller/ClientController.java
@@ -2,6 +2,8 @@ package com.app.nice_roller.controller;
 
 import com.app.nice_roller.dto.ClientDTO;
 import com.app.nice_roller.service.IClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,11 +11,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
 @RequestMapping("/clients")
+@Tag(name = "Client", description = "Gestion des clients")
 public class ClientController {
 
     @Autowired
     private IClientService clientService;
-
+    @Operation(summary = "Récupère tous les clients")
     @GetMapping
     public ResponseEntity<List<ClientDTO>> getAllClients() {
         return ResponseEntity.ok(clientService.getAllClients());
